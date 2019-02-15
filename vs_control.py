014f4a683bc68effeb0238cbee7403e3ab9534bf
Josh Smith <robotkid450@gmail.com>
2019-02-15 01:49:33 -0600

    added power and source set dictionaries

--- vs_control.py
+++ vs_control.py
@@ -5,6 +5,21 @@ class scaller_conection:
     def __init__(self, serialport):
         self.rawPort = serialport
         self.baud = 19200
+        self.commandSetPower = {
+            "Off" : "s power 0",
+            "On" : "s power 1"
+            }
+
+        self.comandSetSource = {
+            "CV" : "s source 0",
+            "SV" : "s source 1",
+            "COMP" : "s source 2",
+            "PC" : "s source 3",
+            "VGA" : "s source 3",
+            "HDMI" : "s source 4"
+            }
+
+
 
     def conect(self)
         #attempts to connect to scaler
