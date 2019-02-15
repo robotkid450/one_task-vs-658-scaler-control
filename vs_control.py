c362f7740258f9bd42a2e21bcca88ea964adb04e
Josh Smith <robotkid450@gmail.com>
2019-02-15 02:01:46 -0600

    added set output dict and minor spelling fixes.

--- vs_control.py
+++ vs_control.py
@@ -1,11 +1,11 @@
 import serial
 
-class scaller_conection:
+class scaler_conection:
 
     def __init__(self, serialport):
         self.rawPort = serialport
         self.baud = 19200
-        self.commandSetPower = {
+        self.comandSetPower = {
             "Off" : "s power 0",
             "On" : "s power 1"
             }
@@ -19,9 +19,32 @@ class scaller_conection:
             "HDMI" : "s source 4"
             }
 
+        self.comandSetOutput = {
+            "NATIVE" : "s output 0",
+            "VGA" : "s output 1",
+            "SVGA" : "s output 2",
+            "XGA" : "s output 3",
+            "SXGA" : "s output 4",
+            "UXGA" : "s output 5",
+            "480I" : "s output 6",
+            "480P" : "s output 7",
+            "720P60" : "s output 8",
+            "1080I60" : "s output 9",
+            "1080P60" : "s output 10",
+            "576I60" : "s output 11",
+            "576P60" : "s output 12",
+            "720P50" : "s output 13",
+            "1080I50" : "s output 14",
+            "1080P50" : "s output 15",
+            "WXGA" : "s output 16",
+            "WSXGA" : "s output 17",
+            "WUXGA" : "s output 18",
+            "WXGA+" : "s output 19"
+            }
 
 
-    def conect(self)
+
+    def conect(self):
         #attempts to connect to scaler
         try:
             self.port = serial.Serial(self.rawPort, self.baud, timeout=1)
